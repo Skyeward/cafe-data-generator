9cf6ba5c5270308dcef40c4bbb91e387ee1791e4
Skyeward <mattyarnold@hotmail.co.uk>
2020-11-07 17:08:38 +0000

    order and purchase counts are functional

--- research/get_cafe_stats.py
+++ research/get_cafe_stats.py
@@ -5,8 +5,13 @@ def define_file_list():
     files = []
     files.append("aberdeen_11-10-2020_19-49-26.csv")
     files.append("chesterfield_05-11-2020_14-04-23.csv")
+
+    files_with_directory = []
+
+    for file_ in files:
+        files_with_directory.append("data_files/" + file_)
     
-    return files
+    return files_with_directory
 
 
 def get_raw_text(filepath):
@@ -21,15 +26,37 @@ def get_raw_text(filepath):
     return line_list
 
 
-def print_stats(file_, text):
-    print(file_)
+def print_stats(file_name, file_text_line_list):
+    print("-----")
+    print(file_name.replace("data_files/", ""))
+    print("-----")
 
-    #find some stats
+    number_of_orders = 0
+    number_of_drink_purchases = 0
+    number_of_large = 0
+    number_of_regular = 0
+    
+    #NUMBER OF ORDERS AND DRINK PURCHASES
+    number_of_orders = len(file_text_line_list)
+
+    price_count = 0
+
+    for line in file_text_line_list:
+        price_count += line[3].count(".") #[3] is purchases, and all drink prices contain a dot, thus finding drink count
+
+    number_of_drink_purchases = price_count
+
+
+
+    #PRINTING STATS
+    print("number of orders: " + str(number_of_orders))
+    print("number of individual drinks purchased: " + str(number_of_drink_purchases))
+    print()
 
 
 if __name__ == "__main__":
     files = define_file_list()
 
     for file_ in files:
-        text = get_raw_text(file)
+        text = get_raw_text(file_)
         print_stats(file_, text)
