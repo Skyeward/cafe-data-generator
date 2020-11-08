4c785319b968025addc812de1c4b2af1a9bd30d4
Skyeward <mattyarnold@hotmail.co.uk>
2020-11-08 14:00:50 +0000

    small cleanup

--- csv_generator.py
+++ csv_generator.py
@@ -2,18 +2,7 @@ import requests
 import json
 
 
-def get_random_name():
-    response = requests.get("https://randomuser.me/api/")
-    response_as_json = response.json()
-
-    name_dict = response_as_json["results"][0]["name"]
-    fname = name_dict["first"]
-    lname = name_dict["last"]
-    print(fname)
-    print(lname)
-
-
-def get_multiple_random_names():
+def get_random_names():
     name_count_to_get = 20
     request_count = 100
     
@@ -23,8 +12,6 @@ def get_multiple_random_names():
 
     fnames = []
     lnames = []
-    removed_fnames = []
-    removed_lnames = []
     index_to_read = 0
 
     while len(fnames) < name_count_to_get and index_to_read < request_count:
@@ -43,18 +30,12 @@ def get_multiple_random_names():
         if is_name_valid == True:
             fnames.append(fname)
             lnames.append(lname)
-        else:
-            removed_fnames.append(fname)
-            removed_lnames.append(lname)
 
         index_to_read += 1
 
     print(fnames)
     print(lnames)
-    print(removed_fnames)
-    print(removed_lnames)
 
 
 if __name__ == "__main__":
-    #get_random_name()
-    get_multiple_random_names()
+    get_random_names()
